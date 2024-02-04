22502a9e848448c6e88b2c52c3703ea30f59a75c
Sam Bhosale <88338885+JutMe@users.noreply.github.com>
2024-02-04 02:09:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
-// Declare the textBoxes array globally
+const firebaseConfig = {
+  apiKey: "AIzaSyBvvuTg6O_L4qtyRcyueFo8x-M9SfDG6sY",
+  authDomain: "capsoteweb.firebaseapp.com",
+  projectId: "capsoteweb",
+  storageBucket: "capsoteweb.appspot.com",
+  messagingSenderId: "514921944177",
+  appId: "1:514921944177:web:2516e643f946e994ccf7d0",
+  measurementId: "G-24V83F2TF2"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
 let textBoxes = [];
 
-// Function to save data to localStorage
 function saveData() {
-  localStorage.setItem('textBoxes', JSON.stringify(textBoxes));
+  const dataRef = database.ref('textboxes');
+  dataRef.set(textBoxes);
 }
 
-// Function to load data from localStorage
 function loadData() {
-  const storedData = localStorage.getItem('textBoxes');
-  if (storedData) {
-    textBoxes = JSON.parse(storedData);
-    renderTextBoxes();
-  }
+  const dataRef = database.ref('textboxes');
+  dataRef.once('value')
+    .then(snapshot => {
+      const storedData = snapshot.val();
+      if (storedData) {
+        textBoxes = storedData;
+        renderTextBoxes();
+      }
+    });
 }
 
-// Function to add a resizable text box
 function addResizableTextBox(section) {
-  // Create elements
   const container = document.createElement("div");
   const textBox = document.createElement("textarea");
   textBox.setAttribute("rows", "4");
   textBox.setAttribute("cols", "50");
   container.appendChild(textBox);
 
-  // Find the target section container
   const titleContainers = document.querySelectorAll('.title-container');
   for (let i = 0; i < titleContainers.length; i++) {
     if (titleContainers[i].textContent.includes(section)) {
@@ -33,27 +46,22 @@ function addResizableTextBox(section) {
     }
   }
 
-  // Push text box data to the array and save
   textBoxes.push({ section: section, value: textBox.value });
   saveData();
 }
 
-// Function to render text boxes from stored data
 function renderTextBoxes() {
   textBoxes.forEach(textBoxData => {
     const container = document.createElement("div");
     const textBox = document.createElement("textarea");
     textBox.value = textBoxData.value;
     container.appendChild(textBox);
-    // Assuming sections have unique IDs:
     document.getElementById(textBoxData.section.toLowerCase().replace(/ /g, '-')).parentNode.insertBefore(container, document.getElementById(textBoxData.section.toLowerCase().replace(/ /g, '-')).nextSibling);
   });
 }
 
-// Load data on page load
 window.addEventListener('load', loadData);
 
-// Attach event listeners to textareas for saving on change
 const textAreas = document.querySelectorAll('textarea');
 textAreas.forEach(textArea => {
   textArea.addEventListener('input', saveData);
